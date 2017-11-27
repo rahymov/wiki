29cf4ddbf62dae4106f3bb0c061242d93f46e427
Yazgylych <y.rahymov@gmail.com>
2017-11-26 19:28:59 -0500

    User authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show]
-
+  before_action :authenticate_user!, except: [:index, :show]
   def new
     @article = current_user.articles.build 
   end
